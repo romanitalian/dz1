89876f4bd2e4691315341638f12b25d3fc205fb4
romanitalian <romanitalian.net@gmail.com>
2017-02-11 19:20:35 +0300

    fix FIXof cooool - I have done IT!

--- build/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  'use strict';
-
-  // setTimeout(function() {
-  //   document.querySelector('.greating_picture').classList.add('m--show');
-  // }, 1000);
-})();
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24oKSB7XHJcbiAgJ3VzZSBzdHJpY3QnO1xyXG5cclxuICAvLyBzZXRUaW1lb3V0KGZ1bmN0aW9uKCkge1xyXG4gIC8vICAgZG9jdW1lbnQucXVlcnlTZWxlY3RvcignLmdyZWF0aW5nX3BpY3R1cmUnKS5jbGFzc0xpc3QuYWRkKCdtLS1zaG93Jyk7XHJcbiAgLy8gfSwgMTAwMCk7XHJcbn0pKCk7Il19
