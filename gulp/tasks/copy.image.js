93caf2e537c5973801f6359d3974e88863ab90b3
romanitalian <romanitalian.net@gmail.com>
2017-02-11 19:23:02 +0300

    fix FIXof cooool - I have done IT!

--- gulp/tasks/copy.image.js
+++ gulp/tasks/copy.image.js
@@ -2,7 +2,7 @@
 
 module.exports = function () {
   $.gulp.task('copy:image', function () {
-    return $.gulp.src('./source/images/**/*.*', {since: $.gulp.lastRun('copy:image')})
+    return $.gulp.src('./source/images/*.*', {since: $.gulp.lastRun('copy:image')})
       .pipe($.gulp.dest($.config.root + '/assets/img'));
   });
 };
--- gulp/tasks/sprite.png.js
+++ gulp/tasks/sprite.png.js
@@ -12,67 +12,13 @@
 //   })
 // };
 
-// module.exports = function() {
-//   $.gulp.task('sprite:png', function() {
-//     var spriteData = $.gulp.src('./source/sprite/*.png')
-//       .pipe($.gp.spritesmith({
-//         imgName: 'sprite.png',
-//         cssName: 'sprite.scss',
-//         cssFormat: 'scss',
-//         imgPath: '../img/sprite/sprite.png',
-//         algorithm: 'binary-tree',
-//         padding: 5,
-//         cssVarMap: function(sprite) {
-//           sprite.name = 's-' + sprite.name
-//         }
-//       }));
-//
-//     spriteData.img.pipe($.gulp.dest($.config.root + '/assets/img/sprite'));
-//     spriteData.css.pipe($.gulp.dest('./app/style'));
-//     return spriteData;
-//   });
-// };
-
-
-
-// module.exports = function() {
-//   $.gulp.task('sprite:png', function() {
-//     var spriteData = $.gulp.src('./source/sprite/*.png')
-//       .pipe($.gp.spritesmith({
-//         imgName: 'sprite.png',
-//         cssName: 'sprite.scss',
-//         cssFormat: 'scss',
-//         imgPath: '../img/sprite/sprite.png',
-//         algorithm: 'binary-tree',
-//         padding: 5,
-//         cssVarMap: function(sprite) {
-//           sprite.name = 's-' + sprite.name
-//         }
-//       }));
-//
-//     spriteData.img.pipe($.gulp.dest($.config.root + '/assets/img/sprite'));
-//     spriteData.css.pipe($.gulp.dest('./app/style'));
-//     return spriteData;
-//   });
-// };
-
-
-
 module.exports = function () {
   $.gulp.task('sprite:png', function () {
-    var spriteData = $.gulp.src('./source/icons/*.png').pipe($.gp.spritesmith({
-      imgName: 'sprite.png', // итоговый спрайт
-      cssName: 'sprite.scss', // файл стилей
-      algorithm: 'left-right',
-      padding: 20
-    }));
-    var imgStream = spriteData.img
-      .pipe($.gulp.dest('./source/images')); // путь куда записываем спрайт
-
-    var cssStream = spriteData.css
-      .pipe($.gulp.dest('./source/style/common')); // путь куда записываем файл стилей для спрайта
-
-    return $.merge(imgStream, cssStream);
-  });
-
+    return $.gulp.src('./source/icons/*.png')
+      .pipe($.gp.spritesmith({
+        imgName: 'sprite.png',
+        cssName: 'sprite.css'
+      }))
+      .pipe($.gulp.dest($.config.root + '/assets/sprite'))
+  })
 };
